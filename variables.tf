9d3907da18c8125082c0092611d71f876ba0cef2
Dimitar <47664474+eggressive@users.noreply.github.com>
2022-09-28 22:47:34 +0200

    remove aws info

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "access_key_id" {
   type      = string
   sensitive = true
-  default   = "AKIAQPPUYRNLBWOAZX4P"
+  default   = ""
 }
 
 variable "secret_key" {
   type      = string
   sensitive = true
-  default   = "aPWrfi/kQ/27DUrH7KslfQPrl83NR2oh1MEAw7Xw"
+  default   = ""
 }
